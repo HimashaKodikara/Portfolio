08a11abacbb73eb7fcf98c0277923373ca2b38e5
19840769 <kodikarah.21@uom.lk>
2024-06-11 20:54:56 +0530

    chore: Update App.jsx and Navbar.jsx components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React ,{useEffect, useState}from 'react';
+import { Link } from 'react-router-dom';
 
+import {styles} from '../styles';
+//import { navLinks } from '../assets';
 const Navbar = () => {
+
+  const [active, setActive] = useState("");
   return (
-    <nav>
-      {/* Add your navbar content here */}
+    <nav className={`
+    ${styles.paddingX} w-full flex
+    items-center py-5 fixed top-0 z-20 bg-primary `}>
+      
+    <div className="flex items-center justify-between w-full mx-auto max-w-7x1">
+      <Link to="/" className="flex items-center gap-2"
+      onClick={() => {
+        setActive("");
+        window.scrollTo(0, 0);
+      }}>
+        <img src={logo} alt="logo" className="object-contain w-9 h-9"/>
+      </Link>
+      </div>
     </nav>
   );
 };
