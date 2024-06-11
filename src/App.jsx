b002348ab1b06eec012e43b08cd7ea904aa300e0
19840769 <kodikarah.21@uom.lk>
2024-06-11 20:28:48 +0530

    chore: Add three.js library as a dependency and update package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+
+import { BrowserRouter } from "react-router-dom";
+
+import {About, Contact, Experience, Feedbacks, Hero, Navbar , Tech,Works, StarsCanvas} from "./components";
 const App = () => {
   return(
-    <div>
-      .D Developer Pro
-      </div>
+   <BrowserRouter>
+     <div className="relative z-0 bg-primary">
+       <div className="bg-center bg-no-repeat bg-cover bg-hero-pattern">
+        <Navbar/>
+        <Hero/>
+          </div>
+     </div>
+   </BrowserRouter>
   )
 }
+
 
 export default App;
