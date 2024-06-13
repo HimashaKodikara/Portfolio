a93ef250bd6f3e7239c94ee08a276a70ccf88884
19840769 <kodikarah.21@uom.lk>
2024-06-13 15:26:47 +0530

    `chore: Update Computers component and add responsive behavior`

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import {Tilt} from 'react-tilt';
+import {motion} from 'framer-motion';	
 
+import {styles} from '../styles';
+import { services } from '../constants';
+import {fadeIn , textVariant} from '../utils/motion';
+
+const ServiceCard =({index,title,icon})=>{
+  return(
+    <p>{title}</p>
+  )
+}
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-    
-    </div>
+    <>
+     <motion.div>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <p className={styles.sectionHeadText}>Overview</p>
+     </motion.div>
+     <motion.p
+     variants={fadeIn("","",0.1,1)}>
+      I'm Undergraduate at University of Moratuwa. I'm quick learner and collaborate closely with clients
+      to create efficient, scalable and user-friendly solutions that solve real-world problems. Let's work together to build something great. 
+     </motion.p>
+     <div className='flex flex-wrap gap-10 mt-20 '>
+      {services.map((service,index) => (
+        <ServiceCard key={services.title}
+         index={index} {...service}/>
+      ))}
+
+     </div>
+    </>
   );
 };
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import {motion} from 'framer-motion';
 import {styles} from '../styles';
 import { ComputersCanvas } from './canvas';
+//import { repeat } from 'maath/dist/declarations/src/misc';
 
 const Hero = () => {
   return (
@@ -21,7 +22,23 @@ const Hero = () => {
     <ComputersCanvas/>
     <div className='flex items-center justify-center w-full absolte xs:bottom-10 bottom-32'>
       
+         <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary
+          flex justify-center items-start p-2'>
+            <motion.dev 
+            animate={{
+              y:[0,24,0]
+            }}
+            transition={{
+              duration:1.5,
+              repeat:Infinity,
+              repeatType :'loop'
+            }
 
+            }
+            className="w-3 h-3 mb-1 rounded-full bg-secondary"/>
+          </div>
+         </a>
     </div>
    </section>
   );
