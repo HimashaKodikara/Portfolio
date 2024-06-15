893d37ebfb7795a33900c4af35f6fc83f8c8427a
19840769 <kodikarah.21@uom.lk>
2024-06-15 22:18:54 +0530

    `chore: Update Tech component to display technology icons dynamically`

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import {BallCanvas} from "./canvas";
+import {SectionWrapper} from "../hoc";
+import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div>
-      {/* Add your tech content here */}
+    <div className="flex flex-wrap justify-center gap-10 felx-row">
+      {technologies.map((technology)=>(
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+          </div>
+      ))}
+
     </div>
   );
 };
