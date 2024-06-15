4feb93608bac95f6593fc25a594fb12d8ef73596
19840769 <kodikarah.21@uom.lk>
2024-06-15 23:47:04 +0530

    `chore: Update Contact component with motion animation and form handling`

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,6 +20,42 @@ const Contact = () => {
      variants={slideIn('left','tween',0.2,1)}
      className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
  <p className={styles.sectionSubText}>Get in touch</p>
+ <h3 className={styles.sectionHeadText}>Contact</h3>
+ <form
+   ref={formRef}
+   onSubmit={handleSubmit}
+   className="flex flex-col gap-8 mt-12">
+    <label className="flex flex-col">
+      <span className="mb-4 font-medium text-white">Your Name</span>
+      <input 
+      type="text"
+      name="name"
+      value={form.name}
+      onChange={handleChange}
+      placeholder="What is your Name?"
+      className="px-6 py-4 font-medium text-white border-none rounded-lg bg-tertiary placeholder:text-secondary outlined-none"/>
+    </label>
+    <label className="flex flex-col">
+      <span className="mb-4 font-medium text-white">Your Name</span>
+      <input 
+      type="text"
+      name="name"
+      value={form.name}
+      onChange={handleChange}
+      placeholder="What is your Name?"
+      className="px-6 py-4 font-medium text-white border-none rounded-lg bg-tertiary placeholder:text-secondary outlined-none"/>
+    </label>
+    <label className="flex flex-col">
+      <span className="mb-4 font-medium text-white">Your Name</span>
+      <input 
+      type="text"
+      name="name"
+      value={form.name}
+      onChange={handleChange}
+      placeholder="What is your Name?"
+      className="px-6 py-4 font-medium text-white border-none rounded-lg bg-tertiary placeholder:text-secondary outlined-none"/>
+    </label>
+   </form>
      </motion.div>
     </div>
   );
