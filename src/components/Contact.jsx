71fd0403646068097696c00a5351077babe86868
19840769 <kodikarah.21@uom.lk>
2024-06-15 23:27:03 +0530

    `chore: Update Contact component with motion animation and form handling`

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,9 +11,18 @@ const Contact = () => {
   const formRef = useRef();
   const[form,setForm] = useState(
     {name:"",email:"",message:""});
+    const [loading,setLoading] = useState(false);
+    const handleChange = (e) => {}
+    const handleSubmit = (e) => {}
   return (
-    
+    <div className="flex flex-col-reverse gap-10 overflow-hidden xl:mt-12 xl:flex-row">
+     <motion.div
+     variants={slideIn('left','tween',0.2,1)}
+     className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
+ <p className={styles.sectionSubText}>Get in touch</p>
+     </motion.div>
+    </div>
   );
 };
 
-export default Contact;
+export default SectionWrapper(Contact,"contact");
